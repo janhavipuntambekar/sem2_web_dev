b9f5aa2f7871a3ab0a6c29636ba40bc24686d000
janhavipuntambekar <janhavi.puntambekar@gmail.com>
2026-01-30 11:31:17 +0530

    chnages

---
+++ class-3/script.js
@@ -0,0 +1,18 @@
+let str="hello developers cat dog"
+let str1=str.slice(12,10);
+console.log(str1);
+
+
+let str=" Learn JavaScript Functions Arrow IIFE ";
+let str1=str.trim();
+console.log(str1);
+console.log(str1.length);
+console.log(str1.toLowerCase());
+console.log(str1.toUpperCase());
+console.log(str1.substring(6,16));
+console.log(str1.slice(6,16));
+console.log(str1.includes("Function","Arrow","iife"));
+console.log(str1.charAt(6));
+console.log(str1.charCodeAt(0));
+let str2=str1.toLowerCase();
+console.log(str2.includes("javascript"));
